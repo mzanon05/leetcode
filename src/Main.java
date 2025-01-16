241ebe52ee308363af696557ab5dc1ceae8bec49
mzanon05 <matheuszanonnunes98@gmail.com>
2025-01-16 12:48:51 -0300

    criacao do projeto

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import leetcode.Teste;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Teste.teste();
+    }
+
+}
---
+++ src/leetcode/Teste.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class Teste {
+
+    public static void teste() {
+        System.out.println("metodo de teste");
+    }
+
+}
